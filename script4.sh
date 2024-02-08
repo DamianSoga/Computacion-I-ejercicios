36f574f540f88bce1bdd0d03077812eec419cbd0
DamianSoga <159476996+DamianSoga@users.noreply.github.com>
2024-02-08 13:08:50 -0600

    Add files via upload

---
+++ script4.sh
@@ -0,0 +1,13 @@
+# Computacion-I-ejercicios
+# Ejercicios, tareas y proyectos
+
+#Creando Variables
+directorio1=Ex1
+directorio2=Ex2
+directorio3=Ex3
+raiz=practica3
+
+# Creando directorios
+echo 'Creando Directorios'
+mkdir -p $raiz/$directorio1/Fx1/Fx2
+echo 'Generados con exito'
