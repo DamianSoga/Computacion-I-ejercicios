a010466361a5b6dd1c3c405012cba92aae49fef2
DamianSoga <159476996+DamianSoga@users.noreply.github.com>
2024-03-04 14:09:00 -0600

    Add files via upload

---
+++ Tarea2.py
@@ -0,0 +1,25 @@
+#Ejercicio 1
+print("Nombre completo de una persona")
+N = str(input())
+print(N.upper())
+print(N.lower())
+print(N.capitalize())
+
+#Ejercicio 2
+print('¿Cual es tu nombre?')
+Nombre = str(input())
+n = len(Nombre)
+print(Nombre.upper(),' tiene',n," letras")
+
+#Ejercicio 3
+print("Numero de la compañia")
+NumComp = str(input())
+
+
+#Ejercicio 4
+print("Dame una Frase")
+F = str(input())
+print("Dame una vocal")
+V =
+
+#Ejercicio 5
