13b600d0735cd15486550322dadb89367f4b6a4f
DamianSoga <159476996+DamianSoga@users.noreply.github.com>
2024-03-05 14:33:04 -0600

    Add files via upload

--- Tarea2.py
+++ Tarea2.py
@@ -1,25 +1,31 @@
-#Ejercicio 1
-print("Nombre completo de una persona")
-N = str(input())
+#Ejercicio 1 
+print("Nombre completo de una persona") 
+N = str(input()) 
 print(N.upper())
-print(N.lower())
-print(N.capitalize())
+print(N.lower()) 
+print(N.capitalize()) 
 
 #Ejercicio 2
-print('¿Cual es tu nombre?')
-Nombre = str(input())
-n = len(Nombre)
-print(Nombre.upper(),' tiene',n," letras")
+print('¿Cual es tu nombre?') 
+Nombre = str(input()) 
+n = len(Nombre) 
+print(Nombre.upper(),' tiene',n," letras") 
 
-#Ejercicio 3
-print("Numero de la compañia")
-NumComp = str(input())
+#Ejercicio 3 
+print("Introduce el numero completo")
+num = str(input())
+num = num[3:]
+num = num[:-3]
+print(num)
 
+#Ejercicio 4 
+F = str(input("Dame una Frase")) 
+print("Dame una vocal") 
+V = str(input())
+print(F.replace(V,V.upper()))
 
-#Ejercicio 4
-print("Dame una Frase")
-F = str(input())
-print("Dame una vocal")
-V =
-
-#Ejercicio 5
+#Ejercicio 5 
+print("Escribe un correo sin el @ y el dominio") 
+Co = str(input()) 
+Co = Co.split('@')
+print('Tu nuevo correo es: '+Co[0]+'@umich.mx')
