44fbf13d2b7e7221004e7862240786725da11666
DamianSoga <159476996+DamianSoga@users.noreply.github.com>
2024-04-11 13:44:17 -0600

    Add files via upload

--- calendario.py
+++ calendario.py
@@ -1,21 +1,52 @@
+def diaspmes(nombre):
+    nombre=nombre.lower()
+    print(nombre)
+    meses=['','enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembree','diciembre']
+    con31=[1,3,5,7,8,11,12]
+    con30=[4,6,10,9]
+    con28=[2]
+    d31=list(range(1,32))
+    d30=list(range(1,31))
+    d28=list(range(1,29))
+    valor=meses.index(nombre)
+    if valor < 1:
+        return None
+    else:
+        if valor in con31:
+            return d31
+        elif valor==2:
+            return d28
+        else:
+            return d30
+
+def diaspmesv(lismeses):
+    if type(lismeses)==list:
+        l=[]
+        for m in lismeses:
+            n=diaspmes(m)
+            l.append(n)
+        return l
+    else:
+        return diaspmes(lismeses)
+
+
+
 def mesbo(mes,dias):
     x=slice(0,7)
     return " ".join(list(map(str,dias[x])))
 
+if __name__=='__main__':
+    CAL=diaspmesv('febrero')
+    d28=list(range(1,29))
+    mesbo(CAL,d28)
+    print(mesbo)
 
-con31=[1,3,5,7,8,11,12]
-con30=[4,6,10,9]
-d31=list(range(1,32))
-d30=list(range(1,31))
-d28=list(range(1,29))
-meses=['','enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembree','diciembre']
 
-for mes in range(1,13):
-    if con30.count(mes):
-       print('{}-{}'.format(meses[mes],d30)) 
-    elif con31.count(mes):
-        print('{}-{}'.format(meses[mes],d31))
-    else:
-        print('{}-{}'.format(meses[mes],d28))
 
-print(mesbo(mes,d28))
+
+q
+    
+
+
+
+
