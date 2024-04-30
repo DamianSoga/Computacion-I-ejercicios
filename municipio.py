d5c438f34c982f06447e528f1c579189ba912076
DamianSoga <159476996+DamianSoga@users.noreply.github.com>
2024-04-30 13:50:20 -0600

    Add files via upload

--- municipio.py
+++ municipio.py
@@ -1,9 +1,21 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-df=pd.read_csv('datam.csv')
-df.plot(x='Poblacion',
-        y='Municipio',
-        kind='line')
 
-plt.show
+df=pd.read_csv('/home/alumno/Downloads/datam.csv',header=0,delimiter=',',quotechar='"')
+
+a=df['Municipio']
+b=df['Poblacion']
+
+mun=a.to_list()
+pob=b.to_list()
+
+totalpob=sum(list(map(lambda x: int(x.replace(',','')),pob)))
+aporc=[int(i.replace(',',''))/totalpob for i in b[0:10]]
+
+plt.pie(aporc,labels=a[0:10])
+
+#fig, ax = plt.subplots()
+#ax.bar(x = a[0:10], height = b[0:10])
+
+plt.show()
